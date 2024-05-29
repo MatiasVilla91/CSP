f3d77f51bc7772a827c6996df933f51d54a3bf21
Mati <180681mvs@gmail.com>
2024-05-29 02:15:55 -0300

    Cambios en tipografia online

---
+++ nombre_proyecto/src/componentes/contacto.js
@@ -0,0 +1,23 @@
+// Contacto.js
+import React from 'react';
+
+function Contacto() {
+  return (
+
+    <div className='container'>
+    
+     
+     <h2>Contacto</h2>
+     
+     <p className='texto2'> sumate  a mejorar Argentina!</p>
+   
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameBorder="0" marginHeight="0" marginWidth="0">Cargando…</iframe>
+    
+    
+    </div>
+
+     
+  );
+}
+
+export default Contacto;
