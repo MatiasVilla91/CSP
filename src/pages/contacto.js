ba98063909785148087b97ff0fb3d8e2c74e8ec0
Mati <180681mvs@gmail.com>
2024-06-26 17:21:30 -0300

    3er intento de despliegue

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -6,7 +6,7 @@ function Contacto() {
     <div className='contact-container'>    
   
      <p className='texto2'>¡Sumate a mejorar Argentina!</p>
-     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameborder="0" marginheight="0" marginwidth="0"  title="Cuestionario de Contacto">Cargando…</iframe>
     </div>
   );
 }
