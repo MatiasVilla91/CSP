06d6e01af692bcdca89e12d9156cbd72025f1f9d
Mati <180681mvs@gmail.com>
2024-07-01 02:18:58 -0300

    Cotacto form

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -6,22 +6,8 @@ function Contacto() {
    
     <div className='contact'>
   
-  <div class="contact">
-  <div class="section">
-    <div class="icon icon-1"></div>
-    <p>¡Sumate a militar!</p>
-  </div>
-  <div class="section">
-    <div class="icon icon-2"></div>
-    <p>Mandanos tu idea</p>
-  </div>
-  <div class="section">
-    <div class="icon icon-3"></div>
-    <p>Aporta</p>
-  </div>
-</div>
 
-   {/*<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameborder="0" marginheight="0" marginwidth="0"  title="Cuestionario de Contacto">Cargando…</iframe>*/}
+   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameborder="0" marginheight="0" marginwidth="0"  title="Cuestionario de Contacto">Cargando…</iframe>
     </div>
   );
 }
