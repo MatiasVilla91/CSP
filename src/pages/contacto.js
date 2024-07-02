53478834759e3303b8a37f9da02ca4d5af01b9a8
Mati <180681mvs@gmail.com>
2024-07-01 23:29:17 -0300

    Cambios en contactos

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -4,8 +4,9 @@ import React from 'react';
 function Contacto() {
   return (
    
-    <div className='contact'>
-  
+    <div className='contact-container'>
+      <p className='texto2'>¡Sumate a mejorar Argentina!</p>
+      
 
    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOQF7pmb8-yZE9V87wk3EeLqjTvkh7Odp_KapFrFpXF3aMbA/viewform?embedded=true" width="100%" height="605" frameborder="0" marginheight="0" marginwidth="0"  title="Cuestionario de Contacto">Cargando…</iframe>
     </div>
